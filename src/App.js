4e0a9e1737d99bfe5043420d0aea4e11521a12d2
Spazcool <douglas.wrightiii@gmail.com>
2020-08-03 11:06:01 -0400

    add table comp, restructure data, add container wrapper, create dynamic rows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Table from './components/Table.js';
 import './App.css';
 import data from './data/example.json';
 
 class App extends React.Component{
   render(){
-  return (
-    <div className="App">
-      <header className="App-header"/>
-      <Table data={data}/>
-    </div>
-  )};
+    return (
+      <div className="App">
+        <header className="App-header"/>
+        <Container>
+          <Table data={data}/>
+        </Container>
+      </div>
+    )
+  };
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,46 +3,69 @@ import Tables from 'react-bootstrap/Table';
 import '../App.css';
 
 class Table extends React.Component {
-  createRow(data){
+  state = {
+    data : this.props.data,
+    sorted: ''
+  };  
+  
+  createRow(datum, index){
+    const att = Object.keys(datum);
+    const cells = att.map((cell) => (
+      <td key={`${cell}-cell-${index}`}>{datum[cell] === undefined ? 'X' : datum[cell]}</td>
+    ));
+
     return (  
-    <tr key={`${data.role}-${data.id}`}>
-      <td>{data.id}</td>
-      <td>{data.name}</td>
-      <td>{data.role}</td>
-      <td>{data.email}</td>
-      <td>{data.officeNumber}</td>
-      <td>{data.github}</td>
-      <td>{data.school}</td>
-    </tr>
+      <tr key={`${datum.role}-${datum.id}`}>
+        {cells}
+      </tr>
     )
   }
 
+  createHeader(list){
+    const listed = list.map((item) => (<th key={`${item}-header`}>{item}</th>));
+    return (
+      <tr onClick={(event) => this.sortByCol(event.target.closest('th').innerText)}>
+       {listed}
+      </tr>
+    )
+  }
+
+  sortByCol(col){
+    let att;
+    switch(col){
+      case 'Office #':
+        att = 'officeNumber';
+        break;
+      case 'Github Account':
+        att = 'github';
+        break;
+      default:
+        att = col.toLowerCase();
+    }
+    //todo this is broken
+    const sorted = this.state.data === this.state.data.sort((a, b) => (a[att] > b[att]) ? 1 : - 1)
+      ? this.state.data.sort((a, b) => (a[att] < b[att]) ? 1 : + 1)
+      : this.state.data.sort((a, b) => (a[att] > b[att]) ? 1 : - 1);
+    console.log(sorted)
+    this.setState({
+      data: sorted
+    });
+  }
+
   render(){
-    const data = this.props.data;
-    console.log(data)
-    const rows = data.map((datum) => this.createRow(datum))
+    //todo rows & headers are independend, if data comes in that is messy, the table will suck
+    const rows = this.state.data.map((datum, index) => this.createRow(datum, index));
+    const headers = this.createHeader(Object.keys(this.state.data[0]));
 
     return (
-      <div className="App">
-        <header className="App-header"/>
-        
-        <Tables striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Role</th>
-              <th>Email</th>
-              <th>Office #</th>
-              <th>Github Account</th>
-              <th>School</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </Tables>
-      </div>
+      <Tables striped bordered hover variant="dark">
+        <thead>
+          {headers}
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </Tables>
     );
   }
 }
