9b5fb50516590a44cdfd380956ff9192331beee5
Spazcool <douglas.wrightiii@gmail.com>
2020-08-03 23:18:31 -0400

    add icons library

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import {FaSearch} from 'react-icons/fa/'
 import '../App.css';
 
 class SearchBar extends React.Component{
+  // todo create a handle userinput method in app.js, pass that here, pass app.js state to table
   render(){
     return (
       <InputGroup className="mb-3">
         <InputGroup.Prepend>
-          <InputGroup.Text id="inputGroup-sizing-default">Default</InputGroup.Text>
+          <InputGroup.Text id="inputGroup-sizing-default"><FaSearch/></InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
           aria-label="Default"
